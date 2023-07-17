511ccd568641af89e9b50278f3aca7ce3939db49
gibanez1983 <69370583+gibanez1983@users.noreply.github.com>
2023-07-17 20:30:21 -0300

    Add files via upload

---
+++ Layouts/header.php
@@ -0,0 +1,24 @@
+<nav class="custom-navbar navbar navbar navbar-expand-md navbar-dark bg-dark" aria-label="Furni navigation bar">
+	 <div class="container">
+			 <a class="navbar-brand" href="index.html"><img src="imagenes/logo_iot.png"></a>
+
+			 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsFurni" aria-controls="navbarsFurni" aria-expanded="false" aria-label="Toogle navigation">
+					 <span class="navbar-toggler-icon"></span>
+			 </button>
+
+			 <div class="collapse navbar-collapse" id="navbarsFurni">
+					 <ul class="custom-navbar-nav navbar-nav m-auto mb-2 mb-md-0">
+							 <li class="nav-item active">
+									 <a class="nav-link" href="index.html">Home</a>
+							 </li>
+							 <li> <a class="nav-link" href="#">Nosotros</a></li>
+							 <li> <a class="nav-link" href="#">Servicios y prestaciones</a></li>
+							 <li> <a class="nav-link" href="#">Especialidades medicas</a></li>
+							 <li> <a class="nav-link" href="#">Medicina laboral</a></li>
+							 <li> <a class="nav-link" href="#">Conacto</a></li>
+
+					 </ul>
+
+			 </div>
+	 </div>
+</nav>
