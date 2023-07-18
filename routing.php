aa5c8d5e25fb9139fd40993797b4035486fe0aea
gibanez1983 <prescott04@gmail.com>
2023-07-18 17:16:36 -0300

    subo nueva estructura

--- routing.php
+++ routing.php
@@ -6,20 +6,21 @@ if(!isset($rta)) {
 }
 //$rta=comprueba_session();
 if( $rta==0){
-  if(!isset($_GET['menu'])) {
-    $_GET['menu'] ='';
+  if(!isset($_GET['p'])) {
+    $_GET['p'] ='';
   }
-  if(!isset($_GET['sis'])) {
-    $_GET['sis'] ='';
-  }
-  if ($_GET['menu']=='recibos' ) {
-    require_once('recibos/recibos.php');
-  }elseif ($_GET['menu']=='permiso_user' ) {
-    require_once('opciones/usuarios_permisos.php');
-  }elseif ($_GET['menu']=='menu_'.$_GET['sis'] ) {
-    require_once('reportes/'.$_GET['sis'].'/menu_'.$_GET['sis'].'.php');
-    require_once('reportes/'.$_GET['sis'].'/'.$_GET['menu'].'/'.$_GET['menu'].'.php');
+
+  if ($_GET['p']=='nosotros' ) {
+    require_once('paginas/nosotros.html');
+  }elseif ($_GET['p']=='serv_prest' ) {
+    require_once('paginas/serv_prest.php');
+  }elseif ($_GET['p']=='esp_med') {
+    require_once('paginas/esp_med.php');
+  }elseif ($_GET['p']=='med_lab') {
+    require_once('paginas/med_lab.php');
+  }elseif ($_GET['p']=='contacto') {
+    require_once('paginas/contacto.php');
   }
 }
-  require_once('principal.html');
+  require_once('paginas/principal.html');
 ?>
