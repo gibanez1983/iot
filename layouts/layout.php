9c6cddb556c9f05063c62bffe5e1435f8137081c
Vanesa Irala <vane130284.vi@gmail.com>
2023-08-08 19:48:27 -0300

    cambio de footer y carousel

--- layouts/layout.php
+++ layouts/layout.php
@@ -11,9 +11,11 @@
 
     <!-- Archivo CSS Bootstrap 5 -->
     <link href="boostrap5/css/bootstrap.min.css" rel="stylesheet">
-
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/glider-js@1/glider.min.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.12.1/css/all.css" crossorigin="anonymous">
     <!-- Archivo CSS Propio -->
-    <link href="css/estilo.css" rel="stylesheet">
+    <link href="css/nav.css" rel="stylesheet">
+     <link href="css/estilo.css" rel="stylesheet"> 
     <!-- Favicon -->
     <link rel="icon" href="imagenes/favicon.ico">
 
@@ -24,10 +26,8 @@
     <script src="boostrap5/js/bootstrap.bundle.min.js"></script>
       <main>
 
-        <div class="container" >
-
-          <!-- Header -->
-          <header class="d-flex flex-wrap py-3 mb-5 border-bottom">
+        <div class="container-fluid">
+          <header>
             <?php
             require_once('header.html');
             ?>
@@ -43,21 +43,21 @@
 
       </main>
       <!-- Footer -->
-      <footer class="footer mt-auto py-3 bg-light">
+       <footer class="footer mt-auto py-3 bg-light">
       <div class="container">
         <?php
           require_once('footer.html');
         ?>
       </div>
-    </footer>
-    <script >
+    </footer> 
+    <!-- <script >
     $(function(){
           $('.navbar-nav li a').filter(function(){return this.href==location.href}).parent().addClass('activo').siblings().removeClass('activo')
           $('.navbar-nav li a').click(function(){
               $(this).parent().addClass('activo').siblings().removeClass('active')
           })
       })
-    </script>
-
+    </script> -->
+   
   </body>
 </html>
